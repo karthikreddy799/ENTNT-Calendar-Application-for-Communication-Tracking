d7da8aa81422006b666fdc73e0683fb4fd63ce7c
karthikreddy799 <128364065+karthikreddy799@users.noreply.github.com>
2025-01-02 12:20:00 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,24 @@
+// Calendar Application Complete Implementation
+// Framework: React.js
+
+// App.js
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AdminModule from "./modules/AdminModule";
+import UserModule from "./modules/UserModule";
+import ReportingModule from "./modules/ReportingModule";
+import NavBar from "./components/NavBar";
+import './styles.css';
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/admin" element={<AdminModule />} />
+        <Route path="/user" element={<UserModule />} />
+        <Route path="/reporting" element={<ReportingModule />} />
+      </Routes>
+    </Router>
+  );
+}
+export default App;
